da52b1fd4da3e7021d372d588f11816ccb823f8e
pszachta <pszachta@umich.edu>
2024-10-21 18:28:46 -0700

    Added SVM inference

--- generation.py
+++ generation.py
@@ -1,6 +1,8 @@
 import numpy as np
 import random
 import sqlite3
+from sklearn.svm import SVC
+from sklearn import metrics
 
 q_input = 251
 n_input = 30
@@ -27,7 +29,6 @@ def generate_uniformly_random_list(n, q):
 
 # Generates data
 def generate_data():
-    dists = []
     s = generate_uniformly_random_list(n_input, q_input)
     A = []
     B = []
@@ -42,9 +43,6 @@ def generate_data():
         else:
             b = generate_uniformly_random_list(1, q_input)[0]
         B.append(b)
-    print(f"s is {s}")
-    print(f"A is {A}")
-    print(f"B is {B}")
     conn = sqlite3.connect("data.db")
     cur = conn.cursor()
     s_create_query_component = ""
@@ -72,13 +70,70 @@ def generate_data():
             current_point.append(item)
         current_point.append(B[i])
         classification_split_s_A_B.append(current_point)
-    print(f"INSERT INTO data (classification, data_split, {s_insert_query_component}{A_insert_query_component}b) VALUES ({question_marks_component});")
-    print(classification_split_s_A_B)
     cur.executemany(f"INSERT INTO data (classification, data_split, {s_insert_query_component}{A_insert_query_component}b) VALUES ({question_marks_component});", classification_split_s_A_B)
     conn.commit()
     cur.execute(f"SELECT * FROM data;")
-    print(cur.fetchall())
     conn.commit()
     conn.close()
 
+def create_X_train_and_y_train():
+    conn = sqlite3.connect("data.db")
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM data WHERE data_split = 'train';")
+    print()
+    X_train_and_y_train = []
+    for row in cur.fetchall():
+        x_train = list(row[n_input + 2:len(row)])
+        y_train = [row[0]]
+        X_train_and_y_train.append(x_train + y_train)
+    conn.commit()
+    random.shuffle(X_train_and_y_train)
+    print(X_train_and_y_train)
+    X_train = []
+    y_train = []
+    for row in X_train_and_y_train:
+        X_train.append(row[:-1])
+        if row[-1] == "lwe":
+            y_train.append(1)
+        else:
+            y_train.append(0)
+    clf = SVC()
+    clf.fit(X_train, y_train)
+    return clf
+
+def create_X_val_and_y_val(clf):
+    conn = sqlite3.connect("data.db")
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM data WHERE data_split = 'val';")
+    print()
+    X_val_and_y_val = []
+    for row in cur.fetchall():
+        x_val = list(row[n_input + 2:len(row)])
+        y_val = [row[0]]
+        X_val_and_y_val.append(x_val + y_val)
+    conn.commit()
+    random.shuffle(X_val_and_y_val)
+    print(X_val_and_y_val)
+    X_val = []
+    y_val = []
+    for row in X_val_and_y_val:
+        X_val.append(row[:-1])
+        if row[-1] == "lwe":
+            y_val.append(1)
+        else:
+            y_val.append(0)
+    y_pred = list(clf.predict(X_val))
+    print(f"y_val is {y_val}")
+    print(f"y_pred is {y_pred}")
+    accuracy = metrics.accuracy_score(y_val, y_pred)
+    f1_score = metrics.f1_score(y_val, y_pred)
+    precision = metrics.precision_score(y_val, y_pred)
+    recall = metrics.recall_score(y_val, y_pred)
+    print(f"Accuracy is {accuracy}")
+    print(f"f1_score is {f1_score}")
+    print(f"Precision is {precision}")
+    print(f"Recall is {recall}")
+
 generate_data()
+clf = create_X_train_and_y_train()
+create_X_val_and_y_val(clf)
